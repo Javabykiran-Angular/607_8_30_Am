3256e4e013afb437ab5dc2575f63d667f40dd275
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-10-13 10:02:13 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,2 @@
+console.log("Hello world");
+console.log('Welcome to Angular 12 Module By Sumit Raokhande');
---
+++ Typescript/Day03/demo1.ts
@@ -0,0 +1,6 @@
+
+// console.log("Hello world");
+// console.log('Welcome to Angular 12 Module By Sumit Raokhande');
+
+var a1,a2,a3;
+var a$b,a_b;
