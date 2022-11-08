fc41b7b0562df75b30b69266f781df64e5df8604
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-11-08 09:49:46 +0530

    Day05

---
+++ Angular/Day05/eventbinding1/eventbinding1.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventbinding1',
+  templateUrl: './eventbinding1.component.html',
+  styleUrls: ['./eventbinding1.component.css']
+})
+export class Eventbinding1Component implements OnInit {
+
+  count:number=0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+      if(this.count==0){
+        console.log("Event Occur...");
+        this.count++;
+      }  
+  }
+
+  onImageClick(){
+    console.log("on Image click...")
+  }
+
+  onDollerEvent(myevent:any){
+    console.log(myevent);
+    console.log("data is => "+myevent.target.value);
+  }
+
+}
