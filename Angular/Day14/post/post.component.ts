57bdd4e0dbf7432db2f5d7a84cfdc3ea79a510e4
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-11-18 10:05:55 +0530

    Day14

---
+++ Angular/Day14/post/post.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  posts:any[]=[];
+  constructor(private service:HttpService) { }
+
+  ngOnInit(): void {
+    this.service.getPost()
+    .subscribe((response)=>{
+      console.log(response);
+        this.posts=(<any>response);
+    })
+  }
+
+}
