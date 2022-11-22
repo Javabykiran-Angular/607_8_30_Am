d5d14db14991d206a375a5c613f37eefb35fcade
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-11-22 09:52:24 +0530

    Day16

---
+++ Angular/Day16/http.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  url:string="https://jsonplaceholder.typicode.com/posts";
+
+  constructor(private http:HttpClient) { }
+
+
+  getPost(){
+    return (this.http.get(this.url));
+  }
+
+  PostData(obj:any){
+      return (this.http.post(this.url,obj));
+  }
+
+  UpdateData(obj:any){
+    return (this.http.put(`${this.url}/${obj.id}`,obj));
+  }
+
+  DeleteData(id:any){
+    return (this.http.delete(`${this.url}/${id}`))
+  }
+
+
+}
---
+++ Angular/Day16/parent/parent.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+  strdata:string="Hopes so u r enjoying Angular 12 Module";
+
+  jsonObj={
+    id:9,
+    fname:'Sumit',
+    lname:'Raokhande'
+  }
+
+  strdataReceived:string='';
+  arrObjReceived:any[]=[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
